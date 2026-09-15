bfefb8d0e4347d9778ff17678003c178aa58007a
seoyeonleeeeeee <seoyeon9184@naver.com>
2026-09-16 01:02:57 +0900

    UserProfuleController 수정

--- src/main/java/likelion14th/lte/user/controller/UserProfileController.java
+++ src/main/java/likelion14th/lte/user/controller/UserProfileController.java
@@ -9,6 +9,8 @@ import likelion14th.lte.user.service.UserProfileService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,8 +23,9 @@ public class UserProfileController {
     @GetMapping
     @Operation(summary = "유저 프로필 조회",description = "유저아이디를 받아 유저 프로필을 반환하는 api 입니다.")
     public ApiResponse<UserProfileResponse> getUserProfile(
-            @RequestParam Long userId
+            @AuthenticationPrincipal Jwt jwt
     ){
+        Long userId = Long.valueOf(jwt.getSubject());
         UserProfileResponse userProfileResponse = userProfileService.getUserProfile(userId);
 
         return ApiResponse.onSuccess(SuccessCode.OK,userProfileResponse);
